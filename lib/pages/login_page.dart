89ffbd83b1446bbe4c7ace3bf3e94aef0ba8c0ec
rakeputr <rakeputri27@gmail.com>
2025-09-27 22:15:38 +0700

    feat: complete login page UI and functionality

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:latihan_kuis/pages/home_page.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -8,8 +9,117 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  String username = "";
+  String password = "";
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.network(
+              'https://upload.wikimedia.org/wikipedia/commons/thumb/7/79/Flutter_logo.svg/1024px-Flutter_logo.svg.png',
+              height: 200,
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            Text(
+              'Login',
+              style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+            ),
+            Text(
+              'Login untuk mengakses lebih banyak fitur',
+              style: TextStyle(fontSize: 16, color: Colors.grey),
+            ),
+            _usernameField(),
+            _passwordField(),
+            _loginButton(context)
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _usernameField() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      child: TextFormField(
+        enabled: true,
+        onChanged: (value) {
+          username = value;
+        },
+        decoration: InputDecoration(
+          hintText: 'Username',
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.blue),
+            borderRadius: BorderRadius.all(Radius.circular(8.0)),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.blue),
+            borderRadius: BorderRadius.all(Radius.circular(8.0)),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _passwordField() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      child: TextFormField(
+        enabled: true,
+        onChanged: (value) {
+          password = value;
+        },
+        obscureText: true,
+        decoration: InputDecoration(
+          hintText: 'Password',
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.blue),
+            borderRadius: BorderRadius.all(Radius.circular(8.0)),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.blue),
+            borderRadius: BorderRadius.all(Radius.circular(8.0)),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _loginButton(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      width: MediaQuery.of(context).size.width,
+      child: ElevatedButton(
+        onPressed: () {
+          String message = "";
+          if (username == "fulan" && password == "fulan") {
+            setState(() {
+              message = "Login berhasil!";
+            });
+            Navigator.pushReplacement(context,
+                MaterialPageRoute(builder: (context) {
+              return HomePage();
+            }));
+          } else {
+            setState(() {
+              message = "Login gagal";
+            });
+          }
+
+          SnackBar messageBar = SnackBar(content: Text(message));
+          ScaffoldMessenger.of(context).showSnackBar(messageBar);
+        },
+        child: Text('Login'),
+        style: ElevatedButton.styleFrom(
+            minimumSize: Size(double.infinity, 55),
+            foregroundColor: Colors.white,
+            backgroundColor: Colors.blue),
+      ),
+    );
   }
 }
